36a7bb9d6dfba03bef67e1820ec0a91b49f68d41
shibo <shibo@daojia-inc.com>
2022-06-09 11:19:30 +0800

    test

---
+++ src/test/java/thread/exercise/SemaphoreTest.java
@@ -0,0 +1,26 @@
+package thread.exercise;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author by Administrator on 2022/5/7.
+ */
+public class SemaphoreTest{
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                try {
+                    semaphore.acquire();
+                    System.out.println(1);
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();
+                }
+
+            }).start();
+        }
+    }
+}
